69160da0f9562ff32762d407030a17d0dfda6640
xunhaoz@xunhaoz-SER <leo20020529@gmail.com>
2025-09-23 12:19:54 +0800

    add kernel panic module

--- kernel.c
+++ kernel.c
@@ -25,18 +25,17 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
     return (struct sbiret){.error = a0, .value = a1};
 }
 
+
+
 void putchar(char ch) {
     sbi_call(ch, 0, 0, 0, 0, 0, 0, 1 /* Console Putchar */);
 }
 
-void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t *) buf;
-    while (n--)
-        *p++ = c;
-    return buf;
-}
-
 void kernel_main(void) {
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    PANIC("booted!");
+    printf("unreachable here!\n");
+
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
